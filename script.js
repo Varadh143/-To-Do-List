5cabf7a50deee0b2fc21e5250174e394c9a9b0b7
Varadh143 <mpvipin88@gmail.com>
2025-08-03 11:28:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+function addTask() {
+    let input = document.getElementById("task-input");
+    let taskText = input.value.trim();
 
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    // Create new list item
+    let li = document.createElement("li");
+    li.innerText = taskText;
+
+    // Create delete button
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.className = "delete-btn";
+
+    // Delete funtionality
+    delBtn.addEventListener("click", function() {
+        li.remove();
+    })
+
+    // Add button to list item
+    li.appendChild(delBtn)
+
+    // Add list item to UL
+    document.getElementById("task-list").appendChild(li);
+
+    // Clear input
+    input.value = "";
+}
